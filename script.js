81fdab259c966b963f5cbf3acf2e49f7d89260e4
RichieEvans <richiestares@gmail.com>
2012-08-08 22:04:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function expandPhoto() {
+
+   // create overlay and append to page
+   var overlay = document.createElement("div");
+   overlay.setAttribute("id","overlay");
+   overlay.setAttribute("class", "overlay");
+   document.body.appendChild(overlay);
+
+   // create image and append to page
+   var img = document.createElement("img");
+   img.style.width = "40%";
+   img.setAttribute("id","img");
+   img.src = this.getAttribute("data-larger");
+   img.setAttribute("class","overlayimg");
+
+   // click to restore page
+   img.onclick=restore;
+
+   document.body.appendChild(img);
+
+}
+// restore page to normal
+function restore() {
+
+ document.body.removeChild(document.getElementById("overlay"));
+ document.body.removeChild(document.getElementById("img"));
+}
+
+
+window.onload=function() {
+   var imgs = document.getElementsByTagName("img");
+   imgs[0].focus();
+   for (var i = 0; i < imgs.length; i++) {
+     imgs[i].onclick=expandPhoto;
+     imgs[i].onkeydown=expandPhoto;
+   }
+}
